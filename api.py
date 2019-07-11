a9db2483830c0f0593195ca9db5f830d7810f688
david <david.hadrianus@tokiomarine-life.co.id>
2019-07-12 00:52:32 +0700

    WIP: get account balance information api

--- api.py
+++ api.py
@@ -1,5 +1,6 @@
 #!flask/bin/python
 from flask import Flask, request, jsonify
+from utils import *
 
 app = Flask(__name__)
 
@@ -16,6 +17,31 @@ def bcaApiSaldo():
         :return:
         your current saldo
     """
+    if request.method == 'POST':
+
+        username = request.json.get('username', None)
+        password = request.json.get('password', None)
+        if not username:
+            return jsonify({"msg": "Missing username parameter"}), 400
+        if not password:
+            return jsonify({"msg": "Missing password parameter"}), 400
+
+        login = getLogin(username, password)
+        if len(login[1])>0:
+            session = login[0]
+            saldo = getSaldo(session)
+            try:
+                if len(saldo) <1:
+                    return jsonify({'detail':'Something wrong, please try 5 minutes later'})
+                return jsonify({'Info rekening anda':saldo})
+            except:
+                pass
+            session.post('https://m.klikbca.com/authentication.do?value(actions)=logout')
+        return jsonify({'detail': "Wrong Login Information"})
+
+
+    return jsonify({"msg": "Not valid request"})
+
 
 @app.route('/api-bca/transaksi', methods=['POST'])
 def bcaApiTransaksi():
